1a682203e06730e06d00be7906025d638c842cfc
Santana <luan.santana@capgemini.com>
2020-08-02 15:00:57 -0300

    contato

--- assets/contact.php
+++ assets/contact.php
@@ -12,40 +12,40 @@
         if ( empty($name) OR empty($subject) OR empty($lname) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
-            echo "Please complete the form and try again.";
+            echo "Por favor, complete o formulário e tente novamente.";
             exit;
         }
 
         // Set the recipient email address.
         // FIXME: Update this to your desired email address.
-        $recipient = "contactformtestgg@gmail.com";
+        $recipient = "luanss4@gmail.com";
 
         // Set the email subject.
-        $subject = "New contact from $name";
+        $subject = "Novo email de $name";
 
         // Build the email content.
-        $email_content = "First Name: $name\n";
+        $email_content = "Nome: $name\n";
         $email_content .= "Email: $email\n\n";
-        $email_content .= "Message:\n$message\n";
+        $email_content .= "Mensagem:\n$message\n";
 
         // Build the email headers.
-        $email_headers = "From: $name <$email>";
+        $email_headers = "De: $name <$email>";
 
         // Send the email.
         if (mail($recipient, $subject, $email_content, $email_headers)) {
             // Set a 200 (okay) response code.
             http_response_code(200);
-            echo "Thank You! Your message has been sent.";
+            echo "Obrigado! Sua mensagem foi enviada.";
         } else {
             // Set a 500 (internal server error) response code.
             http_response_code(500);
-            echo "Oops! Something went wrong and we couldn't send your message.";
+            echo "Oops! algo errado aconteceu, não conseguimos enviar sua mensagem.";
         }
 
     } else {
         // Not a POST request, set a 403 (forbidden) response code.
         http_response_code(403);
-        echo "There was a problem with your submission, please try again.";
+        echo "Houve um problema com o envio, por favor tente novamente.";
     }
 
 ?>
