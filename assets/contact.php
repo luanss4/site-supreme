f2f9e2c7a0c3e9bf242ae3309d3a6382ab3d6f26
Santana <luan.santana@capgemini.com>
2020-08-02 15:10:28 -0300

    ajustes

--- assets/contact.php
+++ assets/contact.php
@@ -9,7 +9,7 @@
         $message = trim($_POST["massage"]);
 
         // Check that data was sent to the mailer.
-        if ( empty($name) OR empty($subject) OR empty($lname) OR empty($message) OR !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        if ( empty($name) OR empty($email) OR empty($message) {
             // Set a 400 (bad request) response code and exit.
             http_response_code(400);
             echo "Por favor, complete o formulário e tente novamente.";
